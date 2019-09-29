71af0f2dd831647519d00b38fa3b92f3bbb72a8a
neelkamath <neelkamath@icloud.com>
2019-09-29 07:59:18 +0530

    Move from kotlin-test to kotlintest

--- src/test/KWikipediaTest.kt
+++ src/test/KWikipediaTest.kt
@@ -1,76 +1,64 @@
 package com.neelkamath.kwikipedia.test
 
 import com.neelkamath.kwikipedia.*
+import io.kotlintest.inspectors.forAll
+import io.kotlintest.inspectors.forAtLeastOne
+import io.kotlintest.inspectors.forNone
+import io.kotlintest.matchers.collections.shouldContain
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.numerics.shouldBeInRange
+import io.kotlintest.matchers.string.shouldContain
+import io.kotlintest.matchers.string.shouldEndWith
+import io.kotlintest.matchers.string.shouldNotContain
+import io.kotlintest.matchers.string.shouldNotEndWith
+import io.kotlintest.matchers.withClue
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 import kotlinx.coroutines.runBlocking
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
-class SearchTest {
-    @Test
-    fun `Search results for "appl" should include "Apple Inc"`() = runBlocking {
-        val results = search("appl")
-        assertTrue("Apple Inc." in results.map { it.title }, "Results were instead: $results")
+class SearchTest : StringSpec({
+    """Search results for "appl" should include "Apple Inc."""" {
+        search("appl").map { it.title } shouldContain "Apple Inc."
     }
 
-    @Test
-    fun `Searching for seven random pages should return seven pages`() = runBlocking<Unit> {
-        search(7).size.also { assertTrue(it == 7, "There were $it results instead") }
+    "Searching for seven random pages should return seven pages" { search(7) shouldHaveSize 7 }
+
+    "Search results shouldn't include reference pages by default" {
+        search("Go").forAll { it.description shouldNotEndWith referenceDescription }
     }
 
-    @Test
-    fun `Search results shouldn't include reference pages by default`() = runBlocking {
-        val descriptions = search("Go").filter { it.description.endsWith(referenceDescription) }
-        assertTrue(descriptions.isEmpty(), "Reference pages returned: $descriptions")
+    """Searching for "Go" should return reference pages when told to""" {
+        search("Go", allowReferences = true).forAtLeastOne { it.description shouldEndWith referenceDescription }
+    }
+})
+
+class UrlGetterTest : StringSpec({
+    "The correct URL should be gotten for an exact title" {
+        getUrl("Apple") shouldBe "https://en.wikipedia.org/wiki/Apple"
+    }
+})
+
+class PageTest : StringSpec({
+    val page = runBlocking { getPage("Apple Inc.") }
+    val content = page.values.joinToString(" ")
+
+    "Headings should not contain separators" {
+        page.keys.forNone { it shouldContain separator }
     }
 
-    @Test
-    fun `Searching for "Go" should return reference pages when told to`() = runBlocking {
-        val descriptions = search("Go", allowReferences = true).filter { it.description.endsWith(referenceDescription) }
-        assertTrue(descriptions.isNotEmpty(), "Search results didn't include reference pages")
-    }
-}
+    "Sections should not contain separators" { content shouldNotContain separator }
 
-class UrlGetterTest {
-    @Test
-    fun `The correct URL should be gotten for an exact title`() = runBlocking {
-        assertEquals(getUrl("Apple"), "https://en.wikipedia.org/wiki/Apple")
-    }
-}
+    "The page's contents shouldn't include the headings" { content shouldNotContain section }
 
-class PageTest {
-    private val page = runBlocking { getPage("Apple Inc.") }
-    private val content = page.values.joinToString(" ")
-
-    @Test
-    fun `Headings should not contain separators`() = assertTrue(
-        page.keys.none { it.contains(separator) },
-        "Headings containing separators: ${page.keys.filter { it.contains(separator) }}"
-    )
-
-    @Test
-    fun `Sections should not contain separators`() = assertFalse(
-        content.contains(separator), "Content including separator ($separator): $content"
-    )
-
-    @Test
-    fun `The page's contents shouldn't include the headings`() = assertFalse(
-        content.contains(section),
-        "Headings included in the page's contents: ${section.findAll(content).toList().map { it.value }}"
-    )
-
-    @Test
-    fun `The page should be as long as the original`() {
+    "The page should be as long as the original" {
         val originalLength = 106_718
         val acceptableDifference = 10_000
         val range = originalLength - acceptableDifference..originalLength + acceptableDifference
-        assertTrue(
-            content.length in range,
+        withClue(
             """
-            The page should have had $range characters, but had ${content.length} characters instead. We use a range 
-            because the Wikipedia page's contents are prone to change.
-            """.replace(Regex("""\s+"""), " ")
-        )
+            |The page should have had $range characters, but had ${content.length} characters instead. We use a range 
+            |because the Wikipedia page's contents are prone to change.
+            """.trimMargin()
+        ) { content.length shouldBeInRange range }
     }
-}
+})
